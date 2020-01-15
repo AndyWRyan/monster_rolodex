805488c3c12a5680f8d292b999bcb04136d12910
Andrew Ryan <andy@Andrews-MacBook-Pro.local>
2020-01-15 18:30:58 -0500

    adding files for github pages

---
+++ build/precache-manifest.dcce371a9d2a46d862e650b91aa578e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed78c5c12cb7323cbdb1e57d944820fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52166c7486157015627b",
+    "url": "/static/css/main.fc44c2bc.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52166c7486157015627b",
+    "url": "/static/js/main.e0b78d0b.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
